54a95c4e39560855abac2905cd98c41a0260b929
李慢慢 <58302927+lishion98@users.noreply.github.com>
2024-07-16 14:46:47 +0800

    添加项目文件。

---
+++ AttributeDisplay.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppConfigFileRw;
+
+/// <summary>
+/// AttributeDisplay.xaml 的交互逻辑
+/// </summary>
+public partial class AttributeDisplay : UserControl
+{
+    public AttributeDisplay()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        AppConfig.SaveSingleConfig(x_attributeName.Text, x_attributeValue.Text);
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Resources;
+using System.IO;
+
+namespace AppConfigFileRw;
+
+public partial class MainWindow : Window
+{
+    private AppConfig appConfig = AppConfig.GetAppConfig();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        DisplayAppConfigProperties();
+    }
+
+    private void SaveConfig_Click(object sender, RoutedEventArgs e)
+    {
+        string json = File.ReadAllText("./app_config.json");
+        x_fileShow.Text = json;
+    }
+
+    private void DisplayAppConfigProperties()
+    {
+        PropertyInfo[] properties = typeof(AppConfig).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var property in properties)
+        {
+            var showC = new AttributeDisplay();
+
+            showC.x_attributeName.Text = property.Name;
+            showC.x_attributeValue.Text = property.GetValue(appConfig)?.ToString(); // 获取当前属性值并设置 TextBox 文本
+
+            x_group.Children.Add(showC);
+        }
+    }
+
+    
+
+}
